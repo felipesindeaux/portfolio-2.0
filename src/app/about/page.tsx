0aeba82f1c5ea5398f5e9eafbb5261af282738da
felipesindeaux <felipesindeaux@outlook.com>
2025-07-20 13:21:14 -0300

    chore: refactor about section reponsiveness

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,31 +7,33 @@ export default function About() {
     <section className="flex flex-col items-center justify-around w-[100vw] relative h-screen overflow-hidden">
       <CarBackground />
       <div className="radial-gradient flex flex-col items-center justify-around w-[100vw]">
-        <div className="about-container flex flex-col items-center">
-          <FadeIn delay={0.2}>
-            <h1 className="about-title">Ajuste os cintos e prepare-se para</h1>
-          </FadeIn>
-
-          <FadeIn delay={0.5}>
-            <h1 className="about-title mb-4">
-              conhecer minha <span className="text-orange-400">trajetória</span>
-            </h1>
-          </FadeIn>
-          <FadeIn delay={0.8}>
-            <div className="about-text-container flex flex-col items-center justify-between text-justify">
-              <p className="about-text">
+        <div className="flex flex-col items-center">
+          <div className="text-center max-w-[290px] xs:max-w-[95vw] mb-4 text-[25px] xs:text-[32px] md:text-[36px] lg:text-[44px]">
+            <FadeIn delay={0.2}>
+              <h1>Ajuste os cintos e prepare-se para</h1>
+            </FadeIn>
+            <FadeIn delay={0.5}>
+              <h1>
+                conhecer minha{" "}
+                <span className="text-orange-400">trajetória</span>
+              </h1>
+            </FadeIn>
+          </div>
+          <FadeIn className="flex items-center justify-center" delay={0.8}>
+            <div className="flex flex-col gap-4 items-center justify-between text-justify max-w-[600px] xl:max-w-[900px] w-[90%] text-[13px] sm:text-[16px]">
+              <p>
                 Sou Felipe, tenho 24 anos e sou Desenvolvedor Full Stack com
                 mais de 3 anos de experiência na estrada do desenvolvimento.
                 Assim como um carro bem ajustado, busco entregar performance,
                 estilo e precisão em cada projeto que desenvolvo.
               </p>
-              <p className="about-text">
+              <p>
                 Minha paixão por tecnologia anda lado a lado com meu entusiasmo
                 por carros. Aqui, cada linha de código carrega um pouco de quem
                 eu sou — direto, criativo e sempre pronto para acelerar rumo a
                 novos desafios.
               </p>
-              <p className="w-full about-text">
+              <p className="w-full">
                 Este portfólio é a minha garagem digital. Sinta-se à vontade
                 para explorar.
               </p>
--- src/components/FadeIn.tsx
+++ src/components/FadeIn.tsx
@@ -5,14 +5,16 @@ import { motion } from "framer-motion";
 interface Props {
   children: React.ReactNode;
   delay?: number;
+  className?: string;
 }
 
-export function FadeIn({ children, delay = 0 }: Props) {
+export function FadeIn({ children, delay = 0, className }: Props) {
   return (
     <motion.div
       initial={{ opacity: 0, y: 20 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.6, delay }}
+      className={className}
     >
       {children}
     </motion.div>
